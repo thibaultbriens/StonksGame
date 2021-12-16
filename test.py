649bf12dc6c9a62f80c0dcc41236152038329225
thibaultbriens <78799333+thibaultbriens@users.noreply.github.com>
2021-12-16 22:28:26 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,38 +1,19 @@
-import pygame
-from random import randint
+list = [1, 3, 5, 7, 9, 11, 13 , 1, 3, 5, 7, 9, 11, 13 , 1, 3, 5, 7, 9, 11, 13 , 1, 3, 5, 7, 9, 11, 13]
 
-prixBTC = 50000
-monthPricesBTC = []
+'''textfile = open("test.txt", "w")
+for element in list:
+    textfile.write(str(element) + "\n")
+textfile.close()'''
 
-pygame.init()
+places = []
+f = open('test.txt', 'r')
 
-screen = pygame.display.set_mode((1600 , 900) , pygame.FULLSCREEN)
-screen.fill((0 , 0 , 0))
+for prix in f:
+    # remove linebreak which is the last character of the string
+    currentPlace = prix[:-1]
+    # add item to the list
+    places.append(currentPlace)
+    print(currentPlace)
 
-def addMonthPricesBTC(list , price) :
-    if len(list) < 29 :
-        list.append(price) #on rajoute un élément a la liste
-    else :
-        for i in range (len(list) - 1) :
-            list[i] = list[i + 1]
-        list[len(list) - 1] = price
-    print(list)
+print(places)
 
-def variationPrixBTC(i) :
-    global prixBTC
-    for j in range (i) :
-        prixBTC += randint(-100 , 200)
-        addMonthPricesBTC(monthPricesBTC , prixBTC)
-
-variationPrixBTC(30)
-
-continuer = True
-
-while continuer :
-    for event in pygame.event.get() :
-            #si la croix est pressée
-            if event.type == pygame.KEYDOWN :
-                if event.key == pygame.K_F12 :
-                    continuer = False
-
-pygame.quit()
